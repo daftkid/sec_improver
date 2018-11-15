15dcdbea74311937b7cf52d31f608ef0dee14d87
daft_kID <alexandr.mykytenko@gmail.com>
2018-11-15 13:46:08 +0200

    Add metadata for key_scanner toolq

--- python/key_scanner.py
+++ python/key_scanner.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
+__author__ = 'Oleksandr Mykytenko'
+__copyright__ = 'Copyright 2018, Mykytenko O.O.'
+__email__ = ['alexandr.mykytenko@gmail.com']
+__version__ = '0.0.1'
+
 from diploma_lib.main_lib import *
 
+TOOL_NAME = 'key_scanner'
 
-def scanner():
-    debug_mode = True
+
+def scanner(opts):
+    # Read command line arguments
+    if opts.ver:
+        print('IAM Key Scanner ver {}'.format(__version__))
+        print(LOGO)
+        exit(0)
+
+    debug_mode = opts.debug
 
     # Reading environment variables
     if debug_mode: info('Reading ENV vars')
@@ -16,4 +29,5 @@ def scanner():
 
 
 if __name__ == '__main__':
-    scanner()
+    opts = options_parser(TOOL_NAME).parse_args()
+    scanner(opts)
